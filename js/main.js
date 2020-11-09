6baff81d2d98b0acd18a24de9bed00dfb4e512b8
Ivan <delfatti.ivan@gmail.com>
2020-11-09 14:44:20 +0100

    next &  prev functions

--- js/main.js
+++ js/main.js
@@ -3,7 +3,34 @@
  ***************************/
 
  const slider = new Vue ({
+
     el: '#slider',
-    data: {},
-    methods: {},
+
+    data: {
+        indexPhotos: 0,
+        photos: [
+            './img/image1.jpg',
+            './img/image2.jpg',
+            './img/image3.jpg',
+            './img/image4.jpg',
+        ]
+    },
+
+    methods: {
+        next(){
+            this.indexPhotos ++;
+
+            if ( this.indexPhotos > ( this.photos.length - 1 ) ) {
+                this.indexPhotos = 0;
+            };
+        },
+        prev(){
+            this.indexPhotos --;
+
+            if ( this.indexPhotos < 0 ) {
+                this.indexPhotos = this.photos.length - 1;
+            };
+        }
+    },
+
  });
