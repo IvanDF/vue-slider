6898ec288f10ac36c51968a317f9518538864f1d
Ivan <delfatti.ivan@gmail.com>
2020-11-09 15:54:11 +0100

    Add text

--- js/main.js
+++ js/main.js
@@ -14,6 +14,12 @@
             './img/image3.jpg',
             './img/image4.jpg',
         ],
+        photosBio: [
+            'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            'Quo aspernatur, nemo eum velit animi sit officia libero odio natus praesentium',
+            'Aliquam deserunt ex aliquid ab deleniti hic similique provident aut?',
+            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo aspernatur, nemo eum velit animi sit officia libero odio natus praesentium aliquam deserunt ex aliquid ab deleniti hic similique provident aut?',
+        ],
         intervalId: 0,
     },
 
